a1998348f397b21f5669166b249307e959269940
Elliot Bobrow <elliotgreybobrow@gmail.com>
2023-04-25 20:56:36 -0700

    hello tokio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+};
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            process(socket).await;
+        });
+    }
+}
+
+async fn process(mut socket: TcpStream) {
+    let mut buf = Vec::new();
+    let _ = socket.read_buf(&mut buf).await.unwrap();
+    socket.write(&buf).await.unwrap();
 }
